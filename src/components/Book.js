1195e878f0259674d7126879f96c81a2acd08f68
Charles <charles.r.ribeiro@gmail.com>
2019-07-23 02:29:10 -0300

    Livros vindo da API

--- src/components/Book.js
+++ src/components/Book.js
@@ -13,10 +13,12 @@ class Book extends Component{
 
     render()
     {
+
+        console.log(this.props.backgroundImage);
         return(
             <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: this.props.width, height: this.props.height, backgroundImage: this.props.backgroundImage }}></div>
+                            <div className="book-cover" style={{ width: this.props.width, height: this.props.height, backgroundImage: this.props.backgroundImage? "url("+ this.props.backgroundImage+ ")":"error" }}></div>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,15 +1,42 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Book from './Book'
+
 
 class Bookshelf extends Component
 {
     static propTypes = {
-        books: PropTypes.array.isRequired
+        books: PropTypes.array.isRequired,
+        shelfTitle: PropTypes.string.isRequired
+    }
+
+    componentDidMount()
+    {
+        console.log(this.props.books);
+        //debugger;
     }
 
     render(){
         return(
-            "teste"
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+                <div className="bookshelf-books">                
+
+                <ol className="books-grid">
+
+                    {this.props.books.map(book =>
+                    <li>
+                        <Book title={book.title} authors={book.authors[0]} width={128} height={192} backgroundImage={book.imageLinks.smallThumbnail} >
+                    
+                        </Book>
+                    </li>)
+                    
+                    }
+                </ol>
+                </div>
+            </div>
+
+            
         );
     }
 }
